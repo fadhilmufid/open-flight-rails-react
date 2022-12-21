7d8c75b3a328b92d353d9556a171176c59cc83c8
fadhilmufid <mufidfadhil@gmail.com>
2022-12-21 21:23:25 +0700

    Add useEffect, useState, Fragment, and Axios

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React,  {useState, useEffect, Fragment} from 'react';
+import axios from 'axios'
 
 const Airlines = () => {
-    return <div>Airlines#Index</div>
+    const[airlines, SetAirlines] = useState([])
+
+    useEffect(() => {
+
+        axios.get('api/v1/airlines.json')
+        .then( resp => {
+            SetAirlines(resp.data.data)
+        } )
+        .catch( resp => console.log(resp) )
+    }, [airlines.length])
+    
+
+    const list = airlines.map(item=>{
+        return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+    })
+
+    return(
+        <Fragment>
+        <div>Airlines#Show</div>
+        <ul> {list} </ul>
+        </Fragment>
+    )   
+    
 }
+
+
+
 
 export default Airlines
