9244fb99b92f2d24db24136876de92ea9d597ead
fadhilmufid <mufidfadhil@gmail.com>
2022-12-21 22:37:10 +0700

    Add Styled Components and Styling CSS

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,35 @@
 import React,  {useState, useEffect, Fragment} from 'react'
 import axios from 'axios'
 import Airline from './Airline'
+import styled from 'styled-components'
+
+const Home = styled.div`
+    text-align: center;
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+`
+const Header = styled.div`
+    padding: 100px 100px 10px 100px;
+
+    h1{
+        font-size: 42px;
+    }
+
+`
+const Subheader = styled.div`
+    font-weight: 300;
+    font-size: 26px;
+
+`
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    width: 100%;
+    padding: 20px;
+
+`
 
 const Airlines = () => {
     const[airlines, SetAirlines] = useState([])
@@ -25,15 +54,15 @@ const Airlines = () => {
     })
 
     return(
-        <div className='home'>
-            <div className='header'>
+        <Home>
+            <Header>
                 <h1>OpenFlights</h1>
-                <div className='subheader'>Honest, unbiased arline Reviews</div>
-            </div>
-            <div className='grid'>
+                <Subheader>Honest, unbiased arline Reviews</Subheader>
+            </Header>
+            <Grid>
                 {grid}
-            </div>
-        </div>
+            </Grid>
+        </Home>
     )   
     
 }
